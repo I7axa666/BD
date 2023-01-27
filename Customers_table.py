41147c9f4900bad34c16d9b0deb35ee027b331ff
Paul Solovey <p.v.solovey@ya.ru>
2023-01-27 12:52:08 +0300

    Parametrr request is placed in a separate function. Corrected the function of changing the client's data. The customer search function has been adjusted.

--- Customers_table.py
+++ Customers_table.py
@@ -1,135 +1,209 @@
 import psycopg2
-from psycopg2 import Error
 from settings import PASSWORD
+from pprint import pprint
 
-class Db:
+def create_db(conn):
+    with conn.cursor() as cur:
+        cur.execute("""
+                CREATE TABLE IF NOT EXISTS Customers (
+                    id SERIAL PRIMARY KEY,
+                    name    VARCHAR(10) NOT NULL,
+                    surname VARCHAR(20) NOT NULL,
+                    email   VARCHAR(40) NOT NULL
+                );
+                """)
 
-    def command(self, query, tup):
-        conn = psycopg2.connect(database="netology_db",
-                                user="postgres",
-                                password=PASSWORD)
-        with conn.cursor() as cur:
-            try:
-                cur.execute(query.format(table_name=self.table_name), tup)
-                conn.commit()
+        cur.execute("""
+                CREATE TABLE IF NOT EXISTS Phone_numbers (
+                    id SERIAL PRIMARY KEY,
+                    cust_id      INTEGER            NOT NULL REFERENCES Customers(id),
+                    phone_number VARCHAR(12) UNIQUE NOT NULL CHECK (CHAR_LENGTH(phone_number)>=10)
+                );
+                """)
+        conn.commit()
+    return print("Таблица создана")
 
-            except (Exception, Error) as error:
-                conn.rollback()
-                return print("Ошибка отправки в БД", error)
+def add_client(conn, first_name, last_name, email, phones=None):
+    with conn.cursor() as cur:
+        cur.execute("""
+                INSERT INTO Customers(name, surname, email)
+                VALUES (%s, %s, %s)
+                RETURNING id;
+                """, (first_name, last_name, email))
+        client_id = cur.fetchone()
+        if phones != None:
+            cur.execute("""
+                    INSERT INTO phone_numbers(cust_id, phone_number)
+                    VALUES (%s, %s);
+                    """, (client_id, phones))
+        conn.commit()
+    return print("Клиент добавлен!")
 
-            # cur.execute("""SELECT * FROM customers;""")
-            # print(cur.fetchall())
-            # cur.execute("""SELECT * from phone_numbers;""")
-            # print(cur.fetchall())
-            try:
-                for table in cur.fetchall():
-                    print(table)
-            except:
-                    print('Готово')
-            conn.close()
+def add_phone(conn, client_id, phone):
+    with conn.cursor() as cur:
+        cur.execute("""
+                INSERT INTO phone_numbers(cust_id, phone_number)
+                VALUES (%s, %s);
+                """, (client_id, phone))
+        conn.commit()
+    return print("Телефон добавлен!")
 
+def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
+    with conn.cursor() as cur:
+        if first_name != None:
+            cur.execute("""
+                    UPDATE Customers 
+                    SET name=%s
+                    WHERE id=%s;
+                    """, (first_name, client_id))
+        if last_name != None:
+            cur.execute("""
+                    UPDATE Customers 
+                    SET surname=%s
+                    WHERE id=%s;
+                    """, (last_name, client_id))
+        if email != None:
+            cur.execute("""
+                    UPDATE Customers 
+                    SET email=%s
+                    WHERE id=%s;
+                    """, (email, client_id))
+        if phones != None:
+            cur.execute("""
+                    UPDATE Phone_numbers 
+                    SET phone_number=%s
+                    WHERE phone_number = %s;
+                    """, (phones[1], phones[0]))
+        conn.commit()
+    return print("Данные изменены!")
 
-class Table(Db):
-    def __init__(self, table_name):
-        self.table_name = table_name
+def delete_phone(conn, client_id, phone):
+    with conn.cursor() as cur:
+        cur.execute("""
+                DELETE FROM phone_numbers 
+                WHERE cust_id=%s 
+                AND phone_number=%s;
+                """, (client_id, phone))
+        conn.commit()
+    return print("Телефон удален!")
 
-    def create_table(self):
-        self.tup = tuple()
+def delete_client(conn, client_id):
+    with conn.cursor() as cur:
+        cur.execute("""
+                DELETE FROM phone_numbers 
+                WHERE cust_id=%s;
+                """, (client_id,))
+        cur.execute("""
+                DELETE FROM customers 
+                WHERE id=%s;
+                """, (client_id,))
+        conn.commit()
+    return print("Данные о клиенте удалены!")
 
-        self.query = """
-        CREATE TABLE IF NOT EXISTS {table_name} (
-            id SERIAL PRIMARY KEY,
-            cust_name VARCHAR(10) NOT NULL,
-            cust_surname VARCHAR(20) NOT NULL,
-            email VARCHAR(40) NOT NULL
-        );
-        """
-        self.command(self.query, self.tup)
+def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
+    query = """
+            SELECT c.id, name, surname, email, pn.phone_number FROM customers c
+            LEFT JOIN phone_numbers pn ON pn.cust_id = c.id
+            WHERE"""
+    params = []
+    tup = []
+    fields = {'name': first_name, 'surname': last_name, 'email': email, 'phone_number': phone}
+    for key, value in fields.items():
+        if value != None:
+            params.append(key + "=%s")
+            tup.append(value)
+    res = ' AND '.join(params) + ';'
+    q_res = query + ' ' + res
+    q_tup = tuple(tup)
 
-        tup = tuple()
-        self.add_query = """
-        CREATE TABLE IF NOT EXISTS Phone_numbers (
-	        id SERIAL PRIMARY KEY,
-	        cust_id INTEGER NOT NULL REFERENCES Customers(id),
-	        phone_number VARCHAR(12) UNIQUE NOT NULL CHECK (CHAR_LENGTH(phone_number)>=10)
-        );
-        """
-        self.command(self.add_query, self.tup)
-        return
+    with conn.cursor() as cur:
+        cur.execute(q_res.format('customers'), q_tup)
+        pprint(cur.fetchall())
 
-    def add_customer(self):
-        # self.tup = tuple()
-        self.cust_name = input('Введите имя: ')
-        self.cust_surname = input('Введите фамилию: ')
-        self.email = input('Введите email: ')
-        self.tup = (self.cust_name, self.cust_surname, self.email)
-        self.query = """
-        INSERT INTO {table_name}(cust_name, cust_surname, email)
-        VALUES (%s, %s, %s);
-        """
-        self.command(self.query, self.tup)
+def my_command():
+    while True:
+        command = input("Введите команду: ").lower()
+        if command == '':
+            print("\nПрограмма остановлена. Хорошего дня!")
+            break
 
-    def add_number(self):
-        self.phone_number = input('Введите номер телефона: ')
-        self.cust_id = int(input('Введите номер клиента: '))
-        self.tup = (self.cust_id, self.phone_number)
-        self.query = """
-        INSERT INTO phone_numbers(cust_id, phone_number)
-        VALUES (%s, %s);
-        """
-        self.command(self.query, self.tup)
+        with psycopg2.connect(database="netology_db",
+                              user="postgres",
+                              password=PASSWORD) as conn:
+            if command == "c":
+                create_db(conn)
+            elif command == "a_c":
+                first_name = input('Введите имя: ')
+                last_name = input('Введите фамилию: ')
+                email = input('Введите email: ')
+                phone = input('Введите телефон: ')
+                if phone == '':
+                    add_client(conn, first_name, last_name, email)
+                else:
+                    add_client(conn, first_name, last_name, email, phone)
+            elif command == "a_p":
+                client_id = int(input('Введите id клиента: '))
+                phone = input('Введите номер телефона: ')
+                add_phone(conn, client_id, phone)
+            elif command == "c_c":
+                client_id = int(input('Введите id клиента: '))
+                edit_fields = input('что меняем?(name, surname, email, phone указать через пробел): ').split()
+                for fields in edit_fields:
+                    if fields == 'name':
+                        first_name = input("Ведите имя: ")
+                        change_client(conn, client_id, first_name=first_name)
+                    elif fields == 'surname':
+                        last_name = input("Ведите фамилию: ")
+                        change_client(conn, client_id, last_name=last_name)
+                    elif fields == 'email':
+                        email = input("Ведите email: ")
+                        change_client(conn, client_id, email=email)
+                    elif fields == 'phone':
+                        phones = []
+                        phone = input('Введите номер телефона, который нужно изменить: ')
+                        phones.append(phone)
+                        phone = input('Введите новый номер телефона: ')
+                        phones.append(phone)
+                        change_client(conn, client_id, phones=phones)
+                    else:
+                        print("Введено неверное поле")
+                        return
+            elif command == "d_p":
+                client_id = int(input('Введите id клиента: '))
+                phone = input('Введите номер телефона: ')
+                delete_phone(conn, client_id, phone)
+            elif command == "d_c":
+                client_id = int(input('Введите id клиента: '))
+                delete_client(conn, client_id)
+            elif command == "f_c":
+                search_info = input('что известно о клиенте?(name, surname, email, phone указать через пробел): ').split()
+                s_list = []
 
-    def edit_data(self):
-        self.id = int(input('Введите номер клиента: '))
-        self.what1 = input('Введите имя: ')
-        self.what2 = input('Введите фамилию: ')
-        self.what3 = input('Введите email: ')
-        self.tup = (self.what1, self.what2, self.what3, self.id)
-        self.query = """
-        UPDATE {table_name} 
-        SET cust_name=%s, cust_surname=%s, email=%s
-        WHERE id = %s;
-        """
-        self.command(self.query, self.tup)
+                if 'name' in search_info:
+                    first_name = input("Ведите имя: ")
+                    s_list.append(first_name)
+                else:
+                    s_list.append(None)
+                if 'surname' in search_info:
+                    last_name = input("Ведите фамилию: ")
+                    s_list.append(last_name)
+                else:
+                    s_list.append(None)
+                if 'email' in search_info:
+                    email = input("Ведите email: ")
+                    s_list.append(email)
+                else:
+                    s_list.append(None)
+                if 'phone' in search_info:
+                    phone = input("Ведите телефон: ")
+                    s_list.append(phone)
+                else:
+                    s_list.append(None)
+                find_client(conn, *s_list)
+            else:
+                print("Такой команды не существует")
 
-    def delete_phone(self):
-        self.id = int(input('Введите номер клиента: '))
-        self.tup = (self.id,)
-        self.query = """
-        DELETE FROM phone_numbers 
-        WHERE cust_id=%s;
-        """
-        self.command(self.query, self.tup)
-
-    def delete_cust(self):
-        self.id = int(input('Введите номер клиента: '))
-        self.tup = (self.id,)
-        self.query = """
-        DELETE FROM {table_name} 
-        WHERE id=%s;
-        """
-        self.command(self.query, self.tup)
-
-    def find_cust(self):
-        self.ithem = input('Введите составляющую поиска: ')
-        self.tup = (self.ithem, self.ithem, self.ithem, self.ithem)
-        self.query = """
-        SELECT c.id, cust_name, cust_surname, email, phone_number FROM {table_name} c
-        LEFT JOIN phone_numbers pn ON pn.cust_id = c.id
-        WHERE cust_name=%s 
-        OR cust_surname=%s 
-        OR email=%s 
-        OR phone_number=%s;
-        """
-        self.command(self.query, self.tup)
+        conn.close()
 
 if __name__ == '__main__':
-    table =Table('Customers')
-    # table.create_table()
-    # table.add_customer()
-    # table.add_number()
-    # table.edit_data()
-    # table.delete_phone()
-    # table.delete_cust()
-    # table.find_cust()
-
+    my_command()
